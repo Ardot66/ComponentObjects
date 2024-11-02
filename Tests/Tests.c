5452a9d807b1ec1e5e0bf9bc9f53a8483909f473
Gabriel Clay <gabrielwclay@gmail.com>
2024-11-02 22:39:53 +1100

    Added tests for basic component interface systems

--- Tests/Tests.c
+++ Tests/Tests.c
@@ -1,24 +1,77 @@
 #include "ComponentObjects.h"
+#include "Tests.h"
 #include <stdio.h>
 
-INTERFACE_DECLARE(Shape, 
-    void (*Function)(void *);
-)
+#define TEST(expression, onFail, ...) \
+{\
+    const char *expressionString = #expression; \
+    TestsCount++; \
+    if(expression) \
+    {\
+        printf("Test Passed: %s\n", expressionString); \
+        TestsPassed ++;\
+    } \
+    else\
+        printf("Test Failed: %s; " onFail " at %s line %d\n", expressionString, ## __VA_ARGS__, __FILE__, __LINE__);\
+}
+
+#define TESTEND printf("%llu out of %llu tests passed", TestsPassed, TestsCount)
+
+size_t TestsCount = 0;
+size_t TestsPassed = 0;
 
 INTERFACE_DEFINE(Shape)
 
-COMPONENT_DECLARE(Square,
-    size_t Amogus;
-)
-
-void Test(void *_this)
+size_t Rectangle_GetArea(void *self)
 {
-    
+    Rectangle *rectangle = self;
+
+    return rectangle->X * rectangle->Y;
 }
 
-COMPONENT_DEFINE(Square, IMPLEMENT(Shape, .Function = Test))
+COMPONENT_DEFINE(Rectangle, 
+    IMPLEMENT(Shape, .GetArea = Rectangle_GetArea)
+)
+
+size_t Trapezoid_GetArea(void *self)
+{
+    Trapezoid *trapezoid = self;
+
+    return ((trapezoid->BottomLength + trapezoid->TopLength) * trapezoid->Height) / 2;
+}
+
+COMPONENT_DEFINE(Trapezoid,
+    IMPLEMENT(Shape, .GetArea = Trapezoid_GetArea)
+)
 
 int main (int argCount, char **argValues)
 {
-    printf("%p, %p", ((Component *)Test), &Test);
+    Rectangle rectangle;
+    rectangle.X = 1;
+    rectangle.Y = 2;
+
+    Trapezoid trapezoid;
+    trapezoid.BottomLength = 4;
+    trapezoid.TopLength = 6;
+    trapezoid.Height = 3;
+
+    const size_t shapesLength = 2;
+    struct ShapePtr {void *Shape; const Shape *VTable;};
+    struct ShapePtr shapes[shapesLength];
+    
+    shapes[0] = (struct ShapePtr){.Shape = &rectangle, .VTable = VTABLE(Rectangle)->Shape};
+    shapes[1] = (struct ShapePtr){.Shape = &trapezoid, .VTable = VTABLE(Trapezoid)->Shape};
+
+    for(size_t x = 0; x < shapesLength; x++)
+    {
+        void *shape = shapes[x].Shape;
+        size_t area = shapes[x].VTable->GetArea(shape);
+
+        if(x == 0)
+            TEST(area == rectangle.X * rectangle.Y, "")
+        else if(x == 1)
+            TEST(area == ((trapezoid.BottomLength + trapezoid.TopLength) * trapezoid.Height) / 2, "")
+    }
+    
+    TESTEND;
 }
